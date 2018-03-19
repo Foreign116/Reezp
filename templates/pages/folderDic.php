0bfbb630f5654af9090a06fcdf1ff3b354c3deb8
Mehmet Ozdemir <whosforeign@gmail.com>
2018-03-18 23:11:56 -0400

    Update folderDic.php

--- templates/pages/folderDic.php
+++ templates/pages/folderDic.php
@@ -1,5 +1,8 @@
 <?php 
 include '../configs/db.inc.php';
+if(!isset($_SESSION['login'])){
+	header('Location: login.php'); 
+}
 include 'createAccountHeader.php';
 include 'folders.php';
 include 'footer.php';
